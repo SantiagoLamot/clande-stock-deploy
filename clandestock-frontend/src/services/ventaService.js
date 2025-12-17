e8b2f8246e01026d3c103f590061dd87a61000bf
Santiago Lamot <santiagolamot@hotmail.com>
2025-12-17 20:10:08 -0300

    Update ventaService.js

--- clandestock-frontend/src/services/ventaService.js
+++ clandestock-frontend/src/services/ventaService.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "/api";
 
 export const getVentaById = async (idVenta, token) => {
   const res = await fetch(`${BASE_URL}/venta/${idVenta}`, {
