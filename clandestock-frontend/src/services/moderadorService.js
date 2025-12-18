a5aefdf6e24ba37bf68de6ee4657c547175db2a8
Santiago Lamot <santiagolamot@hotmail.com>
2025-12-18 00:47:29 -0300

    Update moderadorService.js

--- clandestock-frontend/src/services/moderadorService.js
+++ clandestock-frontend/src/services/moderadorService.js
@@ -1,6 +1,6 @@
 import axios from '../api/axios'; // usa el interceptor
 
 export const registrarModerador = async (data) => {
-    const response = await axios.post('/api/moderador/register', data);
+    const response = await axios.post('/moderador/register', data);
     return response.data;
 };
